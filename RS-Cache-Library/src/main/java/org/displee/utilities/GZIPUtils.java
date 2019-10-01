5273bbc6fdc55630a94fb23c13bca82ea75d97be
James <bluejag1995@live.co.uk>
2019-10-01 01:48:41 +0100

    Add displee's cache library

--- RS-Cache-Library/src/main/java/org/displee/utilities/GZIPUtils.java
+++ RS-Cache-Library/src/main/java/org/displee/utilities/GZIPUtils.java
@@ -1,15 +1,18 @@
-package com.jagex.util;
+package org.displee.utilities;
+
+import org.displee.io.impl.InputStream;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.util.zip.Inflater;
 
 import org.apache.commons.compress.compressors.gzip.GzipCompressorInputStream;
 import org.apache.commons.compress.compressors.gzip.GzipCompressorOutputStream;
 
 public class GZIPUtils {
 
-	public static byte[] gzipBytes(byte[] input) throws IOException {
+	public static byte[] gzipBytes(byte[] input) {
 		 try (ByteArrayOutputStream bout = new ByteArrayOutputStream();
 				 GzipCompressorOutputStream gzipper = new GzipCompressorOutputStream(bout))
 	        {
@@ -18,10 +21,12 @@ public class GZIPUtils {
 	            gzipper.close();
 
 	            return bout.toByteArray();
+	        } catch(Exception ex) {
+	        	return null;
 	        }
 	}
 
-	public static byte[] unzip(byte[] input) throws IOException  {
+	public static byte[] unzip(byte[] input)  {
 		  try (ByteArrayInputStream bin = new ByteArrayInputStream(input);
 				  GzipCompressorInputStream gzipper = new GzipCompressorInputStream(bin))
 	        {
@@ -34,7 +39,34 @@ public class GZIPUtils {
 			    bin.close();
 			    gzipper.close();
 			  return out.toByteArray();
+	        } catch(Exception ex) {
+	        	return null;
 	        }
+	}
+	
+
+	private static Inflater inflater;
+	public static boolean inflate(InputStream inputStream, byte[] data) {
+		try {
+			if ((inputStream.getBytes()[inputStream.getOffset()] ^ 0xffffffff) != -32 || inputStream.getBytes()[inputStream.getOffset() + 1] != -117) {
+				return false;
+			}
+			if (inflater == null) {
+				inflater = new Inflater(true);
+			}
+			try {
+				inflater.setInput(inputStream.getBytes(), 10 + inputStream.getOffset(), (-10 - inputStream.getOffset() - (8 - inputStream.getBytes().length)));
+				inflater.inflate(data);
+			} catch (Exception exception) {
+				inflater.reset();
+				return false;
+			}
+			inflater.reset();
+		} catch (RuntimeException runtimeException) {
+			runtimeException.printStackTrace();
+		}
+		return true;
 	}
 
 }
+
