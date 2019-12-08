22dc9c5282463c8a34de0b925abaae31b93ff49f
James <bluejag1995@live.co.uk>
2019-12-08 08:15:18 +0000

    Fix skeleton decoding for OSRS

--- Plugins/OSRSPlugin/src/main/java/com/rspsi/plugin/loader/FrameBaseLoaderOSRS.java
+++ Plugins/OSRSPlugin/src/main/java/com/rspsi/plugin/loader/FrameBaseLoaderOSRS.java
@@ -22,20 +22,20 @@ public class FrameBaseLoaderOSRS extends FrameBaseLoader {
 	@Override
 	public FrameBase decode(Buffer buffer) {
 		FrameBase base = new FrameBase();
-		int count = buffer.readUShort();
+		int count = buffer.readUByte();
 		int[] transformationType = new int[count];
 		int[][] vertexGroups = new int[count][];
 		for (int index = 0; index < count; index++) {
-			transformationType[index] = buffer.readUShort();
+			transformationType[index] = buffer.readUByte();
 		}
 
 		for (int label = 0; label < count; label++) {
-			vertexGroups[label] = new int[buffer.readUShort()];
+			vertexGroups[label] = new int[buffer.readUByte()];
 		}
 
 		for (int label = 0; label < count; label++) {
 			for (int index = 0; index < vertexGroups[label].length; index++) {
-				vertexGroups[label][index] = buffer.readUShort();
+				vertexGroups[label][index] = buffer.readUByte();
 			}
 		}
 		base.setCount(count);
