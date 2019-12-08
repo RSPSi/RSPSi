68066df5b504f0435d15733d97c3d0b517b06aed
James <bluejag1995@live.co.uk>
2019-12-08 10:06:08 +0000

    Add reset method for keys pressed

--- Client/src/main/java/com/rspsi/options/KeyboardState.java
+++ Client/src/main/java/com/rspsi/options/KeyboardState.java
@@ -4,23 +4,30 @@ import java.util.List;
 
 import com.google.common.collect.Lists;
 
+import com.jagex.map.SceneGraph;
+import javafx.scene.Scene;
 import javafx.scene.input.KeyCode;
+import lombok.extern.slf4j.Slf4j;
 
-
+@Slf4j
 public class KeyboardState {
 	
 	private static List<KeyCode> currentlyDown = Lists.newCopyOnWriteArrayList();
-	
+
+	public static void reset(){
+		log.info("Reset!");
+		SceneGraph.shiftDown = false;
+		SceneGraph.altDown = false;
+		SceneGraph.ctrlDown = false;
+		currentlyDown.clear();
+	}
 	public static void onKeyDown(KeyCode keyCode) {
 		if(!currentlyDown.contains(keyCode)) {
-
-			System.out.println("PRESS: " + keyCode);
 			currentlyDown.add(keyCode);
 		}
 	}
 	
 	public static void onKeyUp(KeyCode keyCode) {
-		System.out.println("RELEASE: " + keyCode);
 		currentlyDown.remove(keyCode);
 	}
 	
