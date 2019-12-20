86a1290adffc7d51aedffd6df9fcc0b79c4ee021
James <bluejag1995@live.co.uk>
2019-12-20 16:12:25 +0000

    Add text wrapping to debug/fps

--- Client/src/main/java/com/jagex/util/TextRenderUtils.java
+++ Client/src/main/java/com/jagex/util/TextRenderUtils.java
@@ -1,11 +1,14 @@
 package com.jagex.util;
 
+import com.jagex.draw.ImageGraphicsBuffer;
+import com.jagex.draw.ProducingGraphicsBuffer;
+
 import java.awt.Color;
 import java.awt.Graphics2D;
 
 public class TextRenderUtils {
 	
-	public static void renderCenter(Graphics2D g, String text, double x, double y, int colour) {
+	public static int renderCenter(Graphics2D g, String text, double x, double y, int colour) {
 		int xOffset = g.getFontMetrics().stringWidth(text) / 2;
 		int yOffset = g.getFontMetrics().getHeight() / 2;
 		String color = colour  + "";
@@ -14,17 +17,44 @@ public class TextRenderUtils {
 		g.setColor(Color.decode(color));
 		g.drawString(text, (int) (x - xOffset), (int) (y - yOffset));
 		g.setColor(oldColor);
+		return yOffset;
 	}
 	
-	public static void renderLeft(Graphics2D g, String text, double x, double y, int colour) {
+	public static int renderLeft(ImageGraphicsBuffer graphicsBuffer, String text, double x, double y, int colour) {
+		if(text == null || text.isEmpty())
+			return 0;
+		Graphics2D g = graphicsBuffer.getGraphics();
 		int xOffset = g.getFontMetrics().stringWidth(text);
 		int yOffset = g.getFontMetrics().getHeight();
+
 		String color = colour  + "";
 		color = color.replaceFirst("0x", "#");
 		Color oldColor = g.getColor();
 		g.setColor(Color.decode(color));
-		g.drawString(text, (int) (x - xOffset), (int) (y - yOffset));
+		if(xOffset > graphicsBuffer.getWidth()){
+			while(!text.trim().isEmpty()){
+				String s = "";
+
+				for(String part : text.split(" ")) {
+					String mixed = s + part;
+					int width = g.getFontMetrics().stringWidth(mixed);
+					if(width >= graphicsBuffer.getWidth()){
+						break;
+					}
+					s = mixed;
+
+				}
+				g.drawString(text, (int) (x - xOffset), (int) (y - yOffset));
+				yOffset += g.getFontMetrics().getHeight();
+				text = text.replace(s, "").trim();
+			}
+
+		} else {
+			g.drawString(text, (int) (x - xOffset), (int) (y - yOffset));
+		}
+
 		g.setColor(oldColor);
+		return yOffset;
 	}
 
 }
